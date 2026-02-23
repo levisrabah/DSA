36dda2db3d17b60912e83455a0cb6e4b2b03cd83
levisrabah <levisrabah@gmail.com>
2026-02-24 01:47:59 +0300

    adds two numbers and prints the result

---
+++ pr2.py
@@ -0,0 +1,5 @@
+# A program that adds two numbers and prints the result
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+result = num1 + num2
+print(f"sum: {num1} + {num2} = {result}")
